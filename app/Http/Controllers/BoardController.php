8444e9a285bb94ce3a0431bf7a0dca377a2161b4
chibuikeIg <paulchibuikeigweze@gmail.com>
2023-02-06 01:06:12 +0100

    formatting

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Board;
+use Illuminate\Http\Request;
 
 class BoardController extends Controller
 {
-    public function index() {
-
+    public function index()
+    {
         return response()->json(Board::get()->toArray());
-
     }
 
-    public function store(Request $request) {
-
+    public function store(Request $request)
+    {
         $data = $request->validate([
-            'title' => ['required', 'string', 'min:5', 'max:255']
+            'title' => ['required', 'string', 'min:5', 'max:255'],
         ]);
 
         $board = Board::create($data);
@@ -24,8 +23,8 @@ class BoardController extends Controller
         return response()->json($board);
     }
 
-    public function destroy(Board $board) {
-
+    public function destroy(Board $board)
+    {
         $board->delete();
 
         return response()->json($board);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\BoardController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\BoardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
